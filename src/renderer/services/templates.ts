3527f6879d32d329a4d0ddfdbbf3268e5b8e18dd
Francisco Hodge <hello@franciscohodge.com>
2020-03-01 22:06:05 -0500

    Buffer

--- src/renderer/services/templates.ts
+++ src/renderer/services/templates.ts
@@ -3,13 +3,14 @@ import { error } from './utils';
 export const getTemplate = async (templateId: string, extension: string) => {
     const templateRelPath = templateId.split('.').join('/');
     const templatePath = `${templateRelPath}.${extension}`;
+    const noop = () => {};
 
     const template = await import('../themes/' + templatePath);
 
     if (template && template.default) {
-        return template.default;
+        return template.default as any;
     } else {
         error(`Could not find theme "${templateId}"`);
-        return () => {};
+        return noop as any;
     }
 }
