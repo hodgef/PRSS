7718b6ef36d7e177bede38d9ce0a23ad7121ec72
Francisco Hodge <hello@franciscohodge.com>
2020-07-17 00:32:47 -0400

    Build update

--- src/main/index.js
+++ src/main/index.js
@@ -25,18 +25,18 @@ const createMainWindow = () => {
     }
   };
 
-  // if (!isDevelopment) {
-  //   options = {
-  //     ...options,
-  //     devTools: false
-  //   };
-  // }
+  if (!isDevelopment) {
+    options = {
+      ...options,
+      devTools: false
+    };
+  }
 
   const window = new BrowserWindow(options);
 
-  //if (isDevelopment) {
+  if (isDevelopment) {
     window.webContents.openDevTools();
-  //}
+  }
 
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
