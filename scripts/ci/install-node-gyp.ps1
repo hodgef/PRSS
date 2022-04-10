14a1dc31bff82d07d6bbe374bdb0ed9ce6ed4f59
Francisco Hodge <hello@franciscohodge.com>
2022-04-09 20:09:54 -0700

    Reinstate release step

--- scripts/ci/install-node-gyp.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "C:\Program Files\nodejs\node_modules\npm\node_modules\@npmcli\run-script"
-npm install node-gyp@latest
-npx node-gyp --verbose list
-npx node-gyp --verbose install $(node -v)
-npx node-gyp --verbose list
-Get-ChildItem "C:\Users\runneradmin\AppData\Local\node-gyp\Cache\$($(node -v).TrimStart('v'))\include\node"
-Set-Variable VCTargetsPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools"
