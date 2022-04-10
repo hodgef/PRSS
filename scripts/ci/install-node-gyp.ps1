258fd321369ea6ff37d24723ea307698870e9c1c
Francisco Hodge <hello@franciscohodge.com>
2022-04-09 17:14:00 -0700

    Add path

--- scripts/ci/install-node-gyp.ps1
+++ scripts/ci/install-node-gyp.ps1
@@ -3,3 +3,4 @@ npx node-gyp --verbose list
 npx node-gyp --verbose install $(node -v)
 npx node-gyp --verbose list
 Get-ChildItem "C:\Users\runneradmin\AppData\Local\node-gyp\Cache\$($(node -v).TrimStart('v'))\include\node"
+Set-Variable VCTargetsPath="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools"
